b1581b7d9a23f6c97773ddf50d93df8953d65655
Kristopher Johnson <kris@kristopherjohnson.net>
2015-04-07 18:23:26 -0400

    Add files

---
+++ KJMessageTestClient/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  KJMessageTestClient
+//
+//  Created by Kristopher Johnson on 4/7/15.
+//  Copyright (c) 2015 Kristopher Johnson. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
